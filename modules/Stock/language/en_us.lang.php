e0502e843d91edda94d5b81c2f5b45d05effb175
joebordes <joe@tsolucio.com>
2020-02-25 13:25:36 +0100

    initial release

---
+++ modules/Stock/language/en_us.lang.php
@@ -0,0 +1,28 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Stock' => 'Product Stock',
+	'SINGLE_Stock' => 'Product Stock',
+	'Stock ID' => 'Product Stock ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_STOCK_INFORMATION' => 'Stock Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'StockNo' => 'Stock No',
+	'Warehouse' => 'Warehouse',
+	'Producto' => 'Product',
+	'Stock' => 'Stock',
+	'CannotCreate' => 'Sorry, a product can only be once in the same warehouse',
+	'Stock ID' => 'Product Stock ID',
+);
+
+?>
---
+++ modules/Stock/language/es_es.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Stock' => 'Stock Producto',
+	'SINGLE_Stock' => 'Stock Producto',
+	'Stock ID' => 'Stock Producto ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_STOCK_INFORMATION' => 'Información Stock',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+
+	'StockNo' => 'Ref. Stock',
+	'Warehouse' => 'Almacén',
+	'Producto' => 'Producto',
+	'Stock' => 'Stock',
+	'CannotCreate' => 'Lo siento, un producto solo puede estar una vez en un almacén',
+);
+
+?>
